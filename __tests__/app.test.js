a60ed6186abcf505742e3f4bc55ee9c3ce1de9c3
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-03 08:40:30 -0700

    controller, model passing test for returning list of books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,22 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  
+  it('should return list of books', async () => {
+    const res = await request(app).get('/books');
+    const sweetgrass = res.body.find((book) => book.id === '1');
+    expect(res.body.length).toEqual(8);
+    expect(sweetgrass).toHaveProperty('title', 'Braiding Sweetgrass');
   });
+
+
   afterAll(() => {
     pool.end();
   });
